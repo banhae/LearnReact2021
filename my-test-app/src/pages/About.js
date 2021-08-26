cce982141740f15ed073ec7d0db6125b1539e18a
Jinwoo.an <banhae11@gmail.com>
2021-08-27 02:19:26 +0900

    Add: default 3 pages (Home, About, Contact)

---
+++ my-test-app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Bla Bla</p>
+        </div>
+    );
+};
+
+export default About;
