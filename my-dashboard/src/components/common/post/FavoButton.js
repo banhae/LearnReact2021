0489424ac37a09a5af41963159666fab4b44340d
Jinwoo.an <banhae11@gmail.com>
2021-09-11 18:26:34 +0900

    Fix: Use contant color value via json file

--- my-dashboard/src/components/common/post/FavoButton.js
+++ my-dashboard/src/components/common/post/FavoButton.js
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { ReactComponent as Favorite } from 'static/assets/icons/favorite_24dp.svg';
+import Colors from 'static/colors.json'
 /**
  * Common Comp | 즐겨찾기 버튼을 그리는 컴포넌트
  */
 const FavoButton = ({ favorite }) => {
     const [buttonColor, setButtonColor] = useState(null);
 
-    // TODO : (검토필요) 상수 중앙 집중 관리..?
-    const ORANGE = "orange";
-    const GRAY = "gray";
-
     // TODO : 디스패치 콜할 것
     // 버튼 클릭 이벤트
     function onToggledButton() {
         switch (buttonColor) {
-            case ORANGE:
-                setButtonColor(GRAY);
+            case Colors.ORANGE:
+                setButtonColor(Colors.GRAY);
                 break;
-            case GRAY:
-                setButtonColor(ORANGE);
+            case Colors.GRAY:
+                setButtonColor(Colors.ORANGE);
                 break;
             default:
                 break;
@@ -29,9 +26,9 @@ const FavoButton = ({ favorite }) => {
     // useEffect로 첫 로드에 버튼 색깔 설정
     useEffect(() => {
         if (favorite) {
-            setButtonColor(ORANGE);
+            setButtonColor(Colors.ORANGE);
         } else {
-            setButtonColor(GRAY);
+            setButtonColor(Colors.GRAY);
         }
         //favorite 값이 바뀔 때 마다 useEffect를 호출
     }, [favorite]);
